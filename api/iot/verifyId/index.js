ae145aa890ef951c66eddad17b20410b68f83f39
John Bryan Delda <mightypirates174@gmail.com>
2020-12-19 04:53:17 +0800

    hotfix

--- api/iot/verifyId/index.js
+++ api/iot/verifyId/index.js
@@ -1,16 +1,16 @@
 const connectToDatabase = require("../../../utils/mongodb");
 
 module.exports = async (req, res) => {
-  const {uid} = req.query 
-  console.log(uid)
+  const { uid } = req.query;
+  console.log(uid);
 
   const db = await connectToDatabase(process.env.MONGODB_URI);
   const studentCollection = db.collection("students");
   const employeeCollection = db.collection("employee");
- 
-    const student = await studentCollection.findOne({ UID: uid });
-    const employee = await employeeCollection.findOne({ UID: uid });
-    employee || student
-      ? res.status(200).json({ data: "verified" })
-      : res.status(200).json({ data: "not verified" });
+
+  const student = await studentCollection.findOne({ UID: uid });
+  //const employee = await employeeCollection.findOne({ UID: uid });
+  student
+    ? res.status(200).json({ data: "verified" })
+    : res.status(200).json({ data: "not verified" });
 };
