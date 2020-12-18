1e639235fecfbff371f4299b4fa34f60cde6496f
John Bryan Delda <mightypirates174@gmail.com>
2020-12-19 04:21:23 +0800

    edits

--- api/iot/verifyId/[verify].js
+++ api/iot/verifyId/[verify].js
@@ -3,7 +3,7 @@ module.exports = async (req, res) => {
   // Get a database connection, cached or otherwise,
   // using the connection string environment variable as the argument
     const {verify} = req.query
-    
+    console.log(uid)
     console.log(verify);
   const db = await connectToDatabase(process.env.MONGODB_URI);
   const studentCollection = db.collection("students");
--- api/iot/verifyId/index.js
+++ api/iot/verifyId/index.js
@@ -1,13 +1,15 @@
 const connectToDatabase = require("../../../utils/mongodb");
 
 module.exports = async (req, res) => {
+  const {uid} = req.query 
+  console.log(uid)
 
   const db = await connectToDatabase(process.env.MONGODB_URI);
   const studentCollection = db.collection("students");
   const employeeCollection = db.collection("employee");
  
-    const student = await studentCollection.findOne({ UID: "29612150" });
-    const employee = await employeeCollection.findOne({ UID: "12391231" });
+    const student = await studentCollection.findOne({ UID: uid });
+    const employee = await employeeCollection.findOne({ UID: uid });
     employee || student
       ? res.status(200).json({ data: "verified" })
       : res.status(200).json({ data: "not verified" });
