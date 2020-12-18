3594c9e24b53a6225f496777ca28bdab5d86d0ab
John Bryan Delda <mightypirates174@gmail.com>
2020-12-19 06:51:07 +0800

    hotfix

--- api/iot/verifyId/index.js
+++ api/iot/verifyId/index.js
@@ -6,7 +6,6 @@ module.exports = async (req, res) => {
 
   const db = await connectToDatabase(process.env.MONGODB_URI);
   const studentCollection = db.collection("students");
-  const employeeCollection = db.collection("employee");
 
   const student = await studentCollection.findOne({ UID: uid });
   //const employee = await employeeCollection.findOne({ UID: uid });
