6b10726444c70757989f339f8713636caecf5cbe
John Bryan Delda <mightypirates174@gmail.com>
2020-12-21 23:56:09 +0800

    latestTraffic

--- api/monitor/latestTraffic.js
+++ api/monitor/latestTraffic.js
@@ -5,10 +5,9 @@ module.exports = async (req, res) => {
   const trafficCollection = db.collection("traffic");
 
   //const employee = await employeeCollection.findOne({ UID: uid });
-  
-    result = await trafficCollection.find({}).sort({_id:-1}).limit(1);
+  console.log("")
+   const result = await trafficCollection.find({}).sort({_id:-1}).limit(1);
     result ? 
     res.status(200).json({message:"SUCCESS"}): res.status(401).json({message:"Fail"})
   
- 
 };
