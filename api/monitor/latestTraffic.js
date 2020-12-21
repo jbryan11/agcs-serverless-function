300fcfa632ab96ba690ced49c73ae93943a88114
John Bryan Delda <mightypirates174@gmail.com>
2020-12-22 01:07:55 +0800

    latest Traffic

--- api/monitor/latestTraffic.js
+++ api/monitor/latestTraffic.js
@@ -1,13 +1,14 @@
 const connectToDatabase = require("../../utils/mongodb");
 
 module.exports = async (req, res) => {
-  const db = await connectToDatabase(process.env.MONGODB_URI);
+  const db = await connectToDatabase(process.env.MONGO_URL);
   const trafficCollection = db.collection("traffic");
 
   //const employee = await employeeCollection.findOne({ UID: uid });
-  console.log("")
-   const result = await trafficCollection.find({}).sort({_id:-1}).limit(1);
-    result ? 
-    res.status(200).json({message:"SUCCESS"}): res.status(401).json({message:"Fail"})
   
+    const cursor = trafficCollection.find({}).sort({_id:-1}).limit(1);
+    const results = await cursor.toArray()
+    console.log(results.length)
+  res.status(200).json({message:"SUCCESS"});
+ 
 };
